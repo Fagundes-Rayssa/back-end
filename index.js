bb4d3f2c43526e19659036859929a7714d844c84
Fagundes-Rayssa <164500870+Fagundes-Rayssa@users.noreply.github.com>
2025-08-08 12:10:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+const port = 3001
+app.use(express.json())
+const usuarios = []
+app.post('/usuarios', (req, res)=>{
+    const usuario = req.body
+    usuarios.push(usuario)
+    res.send("Usuário cadastrado com sucesso!")
+})
+
+app.get('/usuarios', (req, res)=>{
+    res.send(usuarios)
+})
+
+
+app.get('/ola', (req, res)=>{
+    res.send("Olá 3º DS! The bests")
+})
+
+app.listen(port, ()=>{
+    console.log("API rodando...")
+})
